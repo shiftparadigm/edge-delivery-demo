f2034400ebe88a265176d8f00d6f5dda0f7a06e9
Marcy Sutton-Todd <marcy.sutton.todd@principlestudios.com>
2024-05-17 13:54:38 -0700

    Update styles for mobile / feedback

--- blocks/column-cards/column-cards.js
+++ blocks/column-cards/column-cards.js
@@ -1,14 +1,14 @@
 export default function decorate(block) {
-  const mainHeadingSelector = `
-    div > div > h2,
-    div > div > h3,
-    div > div > h4
-  `;
+  const { firstElementChild } = block;
+  const divTree = firstElementChild.children;
+  const hasMainHeading = divTree.length === 1;
+
+  // add special CSS for heading cell that goes full-width
+  if (hasMainHeading) {
+    firstElementChild.classList.add('column-cards-main-heading-wrap');
+  }
+
   const textContentSelector = '.column-cards > div:last-of-type';
-
-  const headingParent = block.querySelector(mainHeadingSelector).parentNode;
   const textWrapper = block.querySelector(textContentSelector);
-
-  headingParent.classList.add('column-cards-main-heading-wrap');
   textWrapper.classList.add('column-cards-text-wrap');
 }
